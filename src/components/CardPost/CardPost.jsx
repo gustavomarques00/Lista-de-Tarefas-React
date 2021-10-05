ff7a7431ee0ddbddfda9774bc3eadb41e5a6a495
Gustavo <gustavomarquesmm@gmail.com>
2021-10-05 01:53:50 -0300

    Atualização do layout dos botões

--- src/components/CardPost/CardPost.jsx
+++ src/components/CardPost/CardPost.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { ReactComponent as DeleteSVG } from "../../assets/img/delete.svg";
+import { ReactComponent as LikeSVG } from "../../assets/img/like.svg";
+import { ReactComponent as LoveSVG } from "../../assets/img/love.svg";
+import "./Estilo.css";
 
 class CardPost extends React.Component {
-
-    apagar(){
-        const indice = this.props.indice
-        this.props.deletarPost(indice)
-    }
+  apagar() {
+    const indice = this.props.indice;
+    this.props.deletarPost(indice);
+  }
 
   render() {
     return (
       <section className="CardPost">
-        <header className="CardPost_Header">
-          <h3 className="CardPost_Titulo">{this.props.titulo}</h3>
-          <DeleteSVG onClick={this.apagar.bind(this)} />
-        </header>
+        <h3 className="CardPost_Titulo">{this.props.titulo}</h3>
         <p className="CardPost_Conteudo">{this.props.texto}</p>
+        <div className="CardPost_Reacoes">
+          <div className="CardPost_IconesGrupo">
+            <LikeSVG className="CardPost_Icones" />
+          </div>
+          <div className="CardPost_IconesGrupo">
+            <LoveSVG className="CardPost_Icones" />
+          </div>
+          <div className="CardPost_IconesGrupoDiferente">
+            <DeleteSVG
+              className="CardPost_Icones "
+              onClick={this.apagar.bind(this)}
+            />
+          </div>
+        </div>
       </section>
     );
   }
